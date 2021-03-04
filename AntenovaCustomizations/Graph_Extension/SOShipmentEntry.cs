cb6977990c799cefd535399e6a3b7035d4b1a885
WayneLin <wayne.lin@lumina-tech.com>
2021-03-04 14:55:36 +0800

    新增 COC Report 功能+Report

--- AntenovaCustomizations/Graph_Extension/SOShipmentEntry.cs
+++ AntenovaCustomizations/Graph_Extension/SOShipmentEntry.cs
@@ -2,11 +2,40 @@ using PX.Data;
 using PX.Data.BQL;
 using PX.Data.BQL.Fluent;
 using PX.Objects.CS;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace PX.Objects.SO
 {
     public class SOShipmentEntry_Extension : PXGraphExtension<SOShipmentEntry>
     {
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            Base.report.AddMenuAction(COCReport);
+        }
+
+        #region Action
+
+        public PXAction<SOShipment> COCReport;
+        [PXButton]
+        [PXUIField(DisplayName = "Print COC Report", Enabled = true, MapEnableRights = PXCacheRights.Select)]
+        protected virtual IEnumerable cOCReport(PXAdapter adapter)
+        {
+            var _reportID = "lm601000";
+            if (Base.Document.Current != null)
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+                parameters["ShipmentNbr"] = Base.Document.Current.ShipmentNbr;
+                parameters["ShipmentType"] = Base.Document.Current.ShipmentType;
+                parameters["_CompanyID"] = PXAccess.GetBranchID().ToString();
+                throw new PXReportRequiredException(parameters, _reportID, string.Format("Report {0}", _reportID));
+            }
+            return adapter.Get();
+        }
+        #endregion
+
         #region Event Handlers
         protected virtual void _(PXCache sender, PXRowSelectedEventArgs e)
         {
