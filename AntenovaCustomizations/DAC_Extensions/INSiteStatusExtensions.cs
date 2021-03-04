b5e83dad38bebb0e5369586383b14a4c9c7412e6
Administrator <Administrator@EC2AMAZ-MOES1C8>
2021-03-04 06:47:25 +0000

    New customization for qty credit hold. Add new dummy field in standard DAC.

---
+++ AntenovaCustomizations/DAC_Extensions/INSiteStatusExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PX.Data;
+using static PX.Objects.SO.SOOrderEntry_Extension;
+
+namespace PX.Objects.IN
+{
+    public class INSiteStatusExt : PXCacheExtension<PX.Objects.IN.INSiteStatus>
+    {
+        #region UsrQtyCreditHold
+        [PXQuantity]
+        [PXUIField(DisplayName = "Qty Credit Hold", Enabled = false)]
+        public virtual decimal? UsrQtyCreditHold 
+        {
+            get
+            {
+                return CalcQtyCreditHold(Base.InventoryID, Base.SiteID);
+            }
+            set { } 
+        }
+        public abstract class usrQtyCreditHold : PX.Data.BQL.BqlDecimal.Field<usrQtyCreditHold> { }
+        #endregion
+    }
+}
