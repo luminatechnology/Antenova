04889db154c62480174b7ab487050613d2737cb1
Administrator <Administrator@EC2AMAZ-MOES1C8>
2021-03-22 05:44:47 +0000

    Add one more report action button on the standard screen.

--- AntenovaCustomizations/Graph_Extension/SOInvoiceEntry.cs
+++ AntenovaCustomizations/Graph_Extension/SOInvoiceEntry.cs
@@ -1,59 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using PX.Common;
-using PX.Api;
 using PX.Data;
 using PX.Objects.AR;
-using PX.Objects.CM;
-using PX.Objects.CR;
-using PX.Objects.CS;
-using PX.Objects.EP;
-using PX.Objects.GL;
-using PX.Objects.IN;
-using PX.Objects.TX;
-using PX.Objects.CA;
-using ItemLotSerial = PX.Objects.IN.Overrides.INDocumentRelease.ItemLotSerial;
-using SiteLotSerial = PX.Objects.IN.Overrides.INDocumentRelease.SiteLotSerial;
-using LocationStatus = PX.Objects.IN.Overrides.INDocumentRelease.LocationStatus;
-using LotSerialStatus = PX.Objects.IN.Overrides.INDocumentRelease.LotSerialStatus;
-using POLineType = PX.Objects.PO.POLineType;
-using POReceipt = PX.Objects.PO.POReceipt;
-using POReceiptType = PX.Objects.PO.POReceiptType;
-using POReceiptLine = PX.Objects.PO.POReceiptLine;
-using SiteStatus = PX.Objects.IN.Overrides.INDocumentRelease.SiteStatus;
-using System.Linq;
-using CRLocation = PX.Objects.CR.Standalone.Location;
-using PX.Objects.AR.CCPaymentProcessing;
-using PX.Objects.AR.CCPaymentProcessing.Common;
-using PX.Objects.AR.CCPaymentProcessing.Helpers;
-using PX.Objects.AR.CCPaymentProcessing.Interfaces;
-using PX.Objects.AR.Repositories;
-using PX.Objects.Common;
-using PX.Objects.Common.Discount;
-using PX.Objects.AR.MigrationMode;
-using PX.Objects.Common.Bql;
-using PX.Common.Collection;
-using PX.Objects.GL.FinPeriods;
-using PX.Objects.Extensions.PaymentTransaction;
-using PX.Data.BQL.Fluent;
-using PX.Data.BQL;
-using PX.Objects;
-using PX.Objects.SO;
 
 namespace PX.Objects.SO
 {
     public class SOInvoiceEntry_Extension : PXGraphExtension<SOInvoiceEntry>
     {
+        public const string ShipmentNoticeRpt = "LM643001";
+
         public override void Initialize()
         {
             base.Initialize();
+
+            Base.report.AddMenuAction(printShipNotice);
             Base.report.AddMenuAction(InvoiceTaiwanReport);
             Base.report.AddMenuAction(InvoiceUKReport);
             Base.report.AddMenuAction(InvoiceSignatureReport);
         }
 
-        #region Event Handlers
+        #region Report Actions
 
         #region Invoice_Taiwan
         public PXAction<SOShipment> InvoiceTaiwanReport;
@@ -108,6 +75,24 @@ namespace PX.Objects.SO
             return adapter.Get();
         }
         #endregion
+
+        public PXAction<ARInvoice> printShipNotice;
+        [PXButton]
+        [PXUIField(DisplayName = "Print Shipment Notice", Enabled = true, MapEnableRights = PXCacheRights.Select)]
+        protected virtual IEnumerable PrintShipNotice(PXAdapter adapter)
+        {
+
+            if (Base.Document.Current != null)
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+                parameters[nameof(ARInvoice.DocType)] = Base.Document.Current.DocType;
+                parameters[nameof(ARInvoice.RefNbr)]  = Base.Document.Current.RefNbr;
+
+                throw new PXReportRequiredException(parameters, ShipmentNoticeRpt);
+            }
+            return adapter.Get();
+        }
 
         #endregion
     }
