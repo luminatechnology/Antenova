515bdddef4ffeccc71ec3bf020b1adcef778c31a
Dennis <nirartha3214@gmail.com>
2021-03-23 16:32:33 +0800

    add new three shipment Notice reports and update Invoice reports

--- AntenovaCustomizations/Graph_Extension/SOInvoiceEntry.cs
+++ AntenovaCustomizations/Graph_Extension/SOInvoiceEntry.cs
@@ -13,11 +13,14 @@ namespace PX.Objects.SO
         public override void Initialize()
         {
             base.Initialize();
-
-            Base.report.AddMenuAction(printShipNotice);
             Base.report.AddMenuAction(InvoiceTaiwanReport);
             Base.report.AddMenuAction(InvoiceUKReport);
             Base.report.AddMenuAction(InvoiceSignatureReport);
+            Base.report.AddMenuAction(printShipNotice);
+            Base.report.AddMenuAction(printTaiwanShipNotice);
+            Base.report.AddMenuAction(printUKShipNotice);
+            Base.report.AddMenuAction(printSignatureShipNotice);
+
         }
 
         #region Report Actions
@@ -40,7 +43,7 @@ namespace PX.Objects.SO
         }
         #endregion
 
-        #region Invoice_Taiwan
+        #region Invoice_UK
         public PXAction<SOShipment> InvoiceUKReport;
         [PXButton]
         [PXUIField(DisplayName = "Print UK Invoice Report", Enabled = true, MapEnableRights = PXCacheRights.Select)]
@@ -58,7 +61,7 @@ namespace PX.Objects.SO
         }
         #endregion
 
-        #region Invoice_Taiwan
+        #region Invoice_Signature
         public PXAction<SOShipment> InvoiceSignatureReport;
         [PXButton]
         [PXUIField(DisplayName = "Print Signature Invoice Report", Enabled = true, MapEnableRights = PXCacheRights.Select)]
@@ -76,6 +79,7 @@ namespace PX.Objects.SO
         }
         #endregion
 
+        #region ShipmentNotice
         public PXAction<ARInvoice> printShipNotice;
         [PXButton]
         [PXUIField(DisplayName = "Print Shipment Notice", Enabled = true, MapEnableRights = PXCacheRights.Select)]
@@ -93,6 +97,67 @@ namespace PX.Objects.SO
             }
             return adapter.Get();
         }
+        #endregion
+
+        #region TaiwanShipmentNotice
+        public PXAction<ARInvoice> printTaiwanShipNotice;
+        [PXButton]
+        [PXUIField(DisplayName = "Print Taiwan Shipment Notice", Enabled = true, MapEnableRights = PXCacheRights.Select)]
+        protected virtual IEnumerable PrintTaiwanShipNotice(PXAdapter adapter)
+        {
+            var _reportID = "LM643002";
+            if (Base.Document.Current != null)
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+                parameters[nameof(ARInvoice.DocType)] = Base.Document.Current.DocType;
+                parameters[nameof(ARInvoice.RefNbr)] = Base.Document.Current.RefNbr;
+
+                throw new PXReportRequiredException(parameters, _reportID, string.Format("Report {0}", _reportID));
+            }
+            return adapter.Get();
+        }
+        #endregion
+
+        #region UKShipmentNotice
+        public PXAction<ARInvoice> printUKShipNotice;
+        [PXButton]
+        [PXUIField(DisplayName = "Print UK Shipment Notice", Enabled = true, MapEnableRights = PXCacheRights.Select)]
+        protected virtual IEnumerable PrintUKShipNotice(PXAdapter adapter)
+        {
+            var _reportID = "LM643003";
+            if (Base.Document.Current != null)
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+                parameters[nameof(ARInvoice.DocType)] = Base.Document.Current.DocType;
+                parameters[nameof(ARInvoice.RefNbr)] = Base.Document.Current.RefNbr;
+
+                throw new PXReportRequiredException(parameters, _reportID, string.Format("Report {0}", _reportID));
+            }
+            return adapter.Get();
+        }
+        #endregion
+
+        #region SignatureShipmentNotice
+        public PXAction<ARInvoice> printSignatureShipNotice;
+        [PXButton]
+        [PXUIField(DisplayName = "Print Signature Shipment Notice", Enabled = true, MapEnableRights = PXCacheRights.Select)]
+        protected virtual IEnumerable PrintSignatureShipNotice(PXAdapter adapter)
+        {
+            var _reportID = "LM643004";
+            if (Base.Document.Current != null)
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+                parameters[nameof(ARInvoice.DocType)] = Base.Document.Current.DocType;
+                parameters[nameof(ARInvoice.RefNbr)] = Base.Document.Current.RefNbr;
+
+                throw new PXReportRequiredException(parameters, _reportID, string.Format("Report {0}", _reportID));
+            }
+            return adapter.Get();
+        }
+        #endregion
 
         #endregion
     }
